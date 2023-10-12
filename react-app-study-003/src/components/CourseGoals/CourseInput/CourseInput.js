4f7e02099809709026e96f59a32491725af3446c
Lucas Amorim <lsamorim.it@gmail.com>
2023-10-11 22:08:44 -0300

    Implement more styling approaches

--- react-app-study-003/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-app-study-003/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+import styles from './CourseInput.module.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -28,11 +29,13 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
-        <input type='text' onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type='submit'>Add Goal</Button>
+      <Button type="submit">Add Goal</Button>
     </form>
   );
 };
