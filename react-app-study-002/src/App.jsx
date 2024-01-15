eee9d24ea577536f8ef7c884cf19b60ee8573d9b
Lucas Amorim <lsamorim.it@gmail.com>
2024-01-15 13:31:04 -0300

    Improve menu behaviour

--- react-app-study-002/src/App.jsx
+++ react-app-study-002/src/App.jsx
@@ -1,13 +1,18 @@
-import { CORE_CONCEPTS } from './data/data';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data/data';
 
 import Header from './components/Header/Header';
 import CoreConceptsSection from './components/Concepts/CoreConceptsSection';
 import TabButton from './components/TabButton/TabButton';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('');
+
   function tabButtonSelected(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
   }
+
   return (
     <div>
       <Header />
@@ -16,11 +21,41 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => tabButtonSelected('components')}>Components</TabButton>
-            <TabButton onSelect={() => tabButtonSelected('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => tabButtonSelected('props')}>props</TabButton>
-            <TabButton onSelect={() => tabButtonSelected('state')}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onSelect={() => tabButtonSelected('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onSelect={() => tabButtonSelected('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onSelect={() => tabButtonSelected('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onSelect={() => tabButtonSelected('state')}
+            >
+              State
+            </TabButton>
           </menu>
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
